cad7b97c0b64d0c6cc69ec682b08ccc501697bbc
Katie Sullivan <SullivanKE@my.lanecc.edu>
2022-05-10 12:55:43 -0700

    Problem 2 finished, renamed problem 1 to be more clear

---
+++ Midterm - 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CS162NMidterm2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Checks if your input is a palindrome.");
+                Console.Write("Please enter a string: ");
+                string userInput = Console.ReadLine();
+                string s = CleanUpString(userInput);
+                if (IsPalindrome(s))
+                    Console.WriteLine("That's a palindrome!");
+                else
+                    Console.WriteLine("That isn't a palindrome.");
+
+            }
+
+            static string CleanUpString(string s)
+            {
+                char[] punctuation = { '.', '\'', ',', '.', '!', '?', ' ' };
+                foreach (char c in punctuation)
+                    while (s.Contains(c))
+                        s = s.Substring(0, s.IndexOf(c)) + s.Substring(s.IndexOf(c) + 1);
+
+                return s.ToLower();
+            }
+
+            static bool IsPalindrome(string s)
+            {
+                if (s.Length <= 1)
+                    return true;
+                else if (s[0] == s[s.Length - 1] && IsPalindrome(s.Substring(1, s.Length - 2)))
+                    return true;
+                return false;
+            }
+        }
+    }
+}
